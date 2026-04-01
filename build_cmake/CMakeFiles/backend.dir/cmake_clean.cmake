fd8f2140b5d2c58a55555e746c3d222352c32154
hemanthvnp <hemantth06@outlook.com>
2026-04-01 08:49:19 +0530

    a

---
+++ build_cmake/CMakeFiles/backend.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backend.dir/src/backend.cpp.o"
+  "CMakeFiles/backend.dir/src/backend.cpp.o.d"
+  "bin/backend"
+  "bin/backend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_cmake/CMakeFiles/gateway.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gateway.dir/src/authenticator.cpp.o"
+  "CMakeFiles/gateway.dir/src/authenticator.cpp.o.d"
+  "CMakeFiles/gateway.dir/src/gateway_main.cpp.o"
+  "CMakeFiles/gateway.dir/src/gateway_main.cpp.o.d"
+  "CMakeFiles/gateway.dir/src/redis_rate_limiter.cpp.o"
+  "CMakeFiles/gateway.dir/src/redis_rate_limiter.cpp.o.d"
+  "bin/gateway"
+  "bin/gateway.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gateway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
